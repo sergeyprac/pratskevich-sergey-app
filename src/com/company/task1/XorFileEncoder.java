4887854a8f41696471dbbbe5821226913645012f
Сергей <sprac94@gmail.com>
2016-10-07 21:05:36 +0300

    update

---
+++ src/com/company/task1/XorFileEncoder.java
@@ -0,0 +1,26 @@
+package com.company.task1;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class XorFileEncoder {
+    public void endcode(String inputFilePath, String outputFilePath) throws IOException {
+
+        FileInputStream fin = new FileInputStream(inputFilePath);
+        FileOutputStream fos = new FileOutputStream(outputFilePath);
+
+        byte[] encoded = new byte[fin.available()];
+        byte[] bytes;
+
+        fin.read(encoded, 0, encoded.length);
+        bytes = encoded.clone();
+
+        for(int i = 1; i < bytes.length; i++)
+            encoded[i] = (byte)(bytes[i] ^ encoded[i - 1]);
+
+        fos.write(encoded, 0, encoded.length);
+        fin.close();
+        fos.close();
+    }
+}
